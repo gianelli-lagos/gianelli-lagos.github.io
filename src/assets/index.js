6d62c2a40277cf2ec6b3bccf640e3bd818a70df0
GianelliL <154636159+GianelliL@users.noreply.github.com>
2025-08-30 22:00:24 -0400

    src assets 1

---
+++ src/assets/index.js
@@ -0,0 +1,65 @@
+import logo from "./logo.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import gmail from "./gmail.webp";
+import linkedin from "./linkedin.png";
+
+import meimage from "./meimage.png";
+
+import css from "./tech/css.png";
+import python from "./tech/python.png";
+import cpp from "./tech/cpp.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import java from "./tech/java.jpg";
+import git from "./tech/git.png";
+import github_dark from "./tech/github_dark.png";
+import ocaml from "./tech/ocaml.png";
+import aws from "./tech/aws.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import coming_soon from "./coming_soon.png";
+
+import dxc from "./company/dxc.png";
+import btt from "./company/btt.png";
+
+export {
+  logo,
+  linkedin,
+  gmail,
+  aws,
+  menu,
+  close,
+  css,
+  python,
+  cpp,
+  java,
+  github,
+  docker,
+  github_dark,
+  ocaml,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  coming_soon,
+  meimage,
+  dxc,
+  btt,
+};
